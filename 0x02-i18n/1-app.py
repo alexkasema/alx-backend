55777f1cd8199500c6840effb85803cbcbade086
Alex Kasema <lexxmulwa@gmail.com>
2024-07-02 02:58:12 -0700

    A Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
+
+from flask_bable import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    """ A configuration class to set up options """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """The default route """
+    return render_template('1-index.html',)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
